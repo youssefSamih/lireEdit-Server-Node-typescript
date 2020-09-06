0ceabe55662c79ba4d95b0f583611c294fb2fd43
Youssef Samih <youssef.samih@4d.com>
2020-09-06 18:44:24 +0100

    feat: until forward tuto

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -38,14 +38,15 @@ export class UserResolver {
       return {
         errors: [
           {
-            field: 'password',
+            field: 'newPassword',
             message: `length must be greater than 3`
           }
         ]
       };
     }
 
-    const userId = await redis.get(FORGET_PASSWORD_PREFIX + token);
+    const key = FORGET_PASSWORD_PREFIX + token;
+    const userId = await redis.get(key);
     if (!userId) {
       return {
         errors: [
@@ -70,7 +71,7 @@ export class UserResolver {
     }
     user.password = await argon2.hash(newPassword);
     await em.persistAndFlush(user);
-
+    await redis.del(key);
     req.session.userId = user?.id;
     return {
       user
